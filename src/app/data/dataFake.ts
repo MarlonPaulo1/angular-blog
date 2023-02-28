5ccf6f0b8cd0412266f926e64924bf4a6e25d93f
Marlon Paulo <marlonpaulo1@gmail.com>
2023-02-28 01:11:14 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover": "https://www.zup.com.br/wp-content/uploads/2021/03/5e32dc5538c14b0d4b28f87f_imagens-angular3-1024x677.png",
+        "title": "Saiu a nova versão do Angular!",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsam repellat"
+    },
+    {
+        "id": "2",
+        "photoCover": "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1096616/retina_1708x683_cover-top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png",
+        "title": "saiu nova verção",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsam repellat"
+    },
+    {
+        "id": "3",
+        "photoCover": "https://www.alura.com.br/artigos/assets/como-comecar-com-angular/como-comecar-com-angular.png",
+        "title": "Angular: O que é, para que serve e vantagens",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsam repellat"
+    },
+    {
+        "id": "4",
+        "photoCover": "https://www.clariontech.com/hubfs/Angular%20Features.png",
+        "title": "Angular Features: What It Brings to Us?",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsam repellat"
+    }
+
+]
